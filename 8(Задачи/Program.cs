3da399dd147ffe95ec33d180d637967b7044adfc
z_moff <zmoffzema@gmail.com>
2024-06-20 00:16:54 +0300

    Добавьте файлы проекта.

---
+++ 8(Задачи/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрос номера операции
+        Console.WriteLine("Введите номер операции:");
+        Console.WriteLine("1. Сложение");
+        Console.WriteLine("2. Вычитание");
+        Console.WriteLine("3. Умножение");
+
+        int operation = Convert.ToInt32(Console.ReadLine());
+
+        // Определение операции по введенному номеру
+        switch (operation)
+        {
+            case 1:
+                Console.WriteLine("Вы выбрали операцию: Сложение");
+                break;
+            case 2:
+                Console.WriteLine("Вы выбрали операцию: Вычитание");
+                break;
+            case 3:
+                Console.WriteLine("Вы выбрали операцию: Умножение");
+                break;
+            default:
+                Console.WriteLine("Операция неопределена");
+                break;
+        }
+    }
+}
